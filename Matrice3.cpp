df051f6df606aa4bf2ea15dda338ba6647a61274
unknown <leonard.lebrun@epfl.ch>
2020-03-05 12:21:43 +0100

    constructeurs

--- Matrice3.cpp
+++ Matrice3.cpp
@@ -1,9 +1,20 @@
 #include "Matrice3.h"
 
-Matrice3::Matrice3()
+Matrice3::Matrice3(): m_c1({1, 0, 0}), m_c2({0, 1, 0}, m_c3({0, 0, 1}))
+
+        /// Le constructeur par défaut renvoie la matrice identité
 {
-    //ctor
 }
+
+Matrice3::Matrice3( double l1c1, double l1c2, double l1c3,
+                    double l2c1, double l2c2, double l2c3,
+                    double l3c1, double l3c2, double l3c3) : m_c1({l1c1, l2c1, l3c1}), m_c2({l1c2, l2c2, l3c2}), m_c3(l1c3, l2c3, l3c3)
+
+        /// Constructeur sur demande
+{
+
+}
+
 
 Matrice3::~Matrice3()
 {
--- Matrice3.h
+++ Matrice3.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #ifndef MATRICE3_H
 #define MATRICE3_H
 
@@ -9,11 +11,22 @@ class Matrice3
     public:
 
         Matrice3();
+
+        Matrice3(double l1c1, double l1c2, double l1c3,
+                 double l2c1, double l2c2, double l2c3,
+                 double l3c1, double l3c2, double l3c3);
+
+
+
+
+
         virtual ~Matrice3();
+
+
 
     protected:
 
-        Vecteur m_c1;
+        Vecteur m_c1;       // La matrice est ordonnées en Vecteurs-colonnes
         Vecteur m_c2;
         Vecteur m_c3;
 
