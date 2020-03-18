e39e80075a522054e3ca4e73648a61ba71504c8c
David-Rossboth <david.rossboth@epfl.ch>
2020-03-18 19:17:01 +0100

    Newmark

--- Integrateur.cpp
+++ Integrateur.cpp
@@ -3,14 +3,67 @@
 
 
 
-void IntegrateurEulerCromer::integre(Toupie& toupie, double const& dt) const {
+void IntegrateurEulerCromer::integre(Integrable& integrable, double const& dt) const {
 
         /// Intègre numériquement selon la méthode d'Euler Cromer
 
 
-    toupie.setVitesse( toupie.getVitesse() + dt * f(m_t, toupie.getParametre(), toupie.getVitesse()) );
+    toupie.setVitesse( toupie.getVitesse() + dt * toupie.equEvol(m_t) );
 
-    toupie.setParametre( toupie.getParametre() + dt * toupie.getVitesse() );
+    toupie.setParam( toupie.getParam() + dt * toupie.getVitesse() );
+
+
+}
+
+
+
+void IntegrateurNewmark::integre(Integrable& integrable, double const& dt, double const& epsilon) const {
+
+        /// Integre numériquement selon la méthode de Newmark
+
+
+    Vecteur s ( integrable.equEvol(m_t) ) ;
+
+    Vecteur P_nmoins1 ( integrable.getParam() ) ;
+
+    Vecteur Ppoint_nmoins1 ( integrable.getVitesse() ) ;
+
+
+    do {
+
+
+        Vecteur q ( integrable.getParam() ) ;
+
+        Vecteur r ( integrable.equEvol(m_t + dt) ) ;
+
+
+        integrable.setVitesse( Ppoint_nmoins1 + dt/2 * (r + s) ) ;
+
+        integrable.setParam( P_nmoins1 + dt * Ppoint_nmoins1 + dt*dt/3 * (1/2 * r + s) ) ;
+
+
+
+        Vecteur dist ( integrable.getParam() - q ) ;
+
+
+    } while ( dist.norme() >= epsilon ) ;
+
+
+}
+
+
+void IntegrateurRK4::integre(Integrable& integrable, double const& dt) const {
+
+
+        /// Integre numériquement selon la méthode Runge-Kutta 4
+
+
+
+
+
+            // A faire éventuellement plus tard
+
+
 
 
 }
