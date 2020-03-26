8beded47dad29052b0ad558e34467318d2ba0df0
David-Rossboth <david.rossboth@epfl.ch>
2020-03-26 13:38:37 +0100

    les cpp lies

--- source/ConeSimple.cpp
+++ source/ConeSimple.cpp
@@ -2,5 +2,17 @@
 
 
 ConeSimple::ConeSimple(Vecteur const& param, Vecteur const& vit, double const& rayon, double const& hauteur, double const& masse)
-        : Toupie(param, vit), m_I1 (3*masse/20*( rayon*rayon+1/4*hauteur*hauteur ) ), m_I3(3*masse/10*rayon*rayon)
+        : Toupie(param, vit, calculeIA1(rayon, hauteur, masse), calculeI3(rayon, masse), masse, 3.0/4.0*hauteur)
 {}
+
+double ConeSimple::calculeIA1(double const& rayon, double const& hauteur, double const& masse) const {
+
+    return 3*masse/20*( rayon*rayon+3.0/5*hauteur*hauteur );
+
+}
+
+double ConeSimple::calculeI3(double const& rayon, double const& masse) const {
+
+    return 3*masse/10*( rayon*rayon);
+
+}
--- source/Toupie.cpp
+++ source/Toupie.cpp
@@ -1,11 +1,56 @@
 #include "../headers/Toupie.h"
-#include "../headers/Vecteur.h"
-#include "../headers/Integrable.h"
+#include <cmath>
 
 
-Toupie::Toupie (Vecteur const& param, Vecteur const& vit)
-    : Integrable(param, vit)
+Toupie::Toupie (Vecteur const& param, Vecteur const& vit, double const& IA1, double const& I3, double const & masse, double const& d)
+    : Integrable(param, vit), m_IA1(IA1), m_I3(I3), m_masse(masse), m_d(d)
 {}
 
+double Toupie::calculeIA1() const{
 
+    return 1.0;
+
+}
+
+double Toupie::calculeI3() const{
+
+    return 3.0;
+
+}
+
+
+Vecteur Toupie::equEvol(double const& temps) const {
+
+    /// Avec l'équation d'évolution en page 12 du complément mathématique, assumant donc distance centre de masse -
+    /// point de contact = cste
+
+
+    Vecteur sortie ;                // convention : (théta, psy, phi)
+                                    // init. au vecteur nul
+
+
+    if (abs(m_IA1) <= 1e-9 ) {
+
+        return sortie;              // pas d'accélération de la rotation s'il n'y a rien à faire tourner !
+
+    } else if (abs(sin(m_P.getCoord(0))) <= 1e-9 ) {
+
+        return sortie;              // voir formule
+
+    }
+
+    sortie.setCoord(0, 1.0/m_IA1*(m_masse*g.norme()*m_d*sin(m_P.getCoord(0))+m_Ppoint.getCoord(1)*sin(m_P.getCoord(0))
+    * ((m_IA1-m_I3)*m_Ppoint.getCoord(1)*cos(m_P.getCoord(0))-m_I3*m_Ppoint.getCoord(2) )) ) ;
+
+    sortie.setCoord(1, m_Ppoint.getCoord(0)/(m_IA1*sin(m_P.getCoord(0))) * ((m_I3-2*m_IA1)*m_Ppoint.getCoord(1)*cos(m_P.getCoord(0))
+    + m_I3*m_Ppoint.getCoord(2) ) ) ;
+
+    sortie.setCoord(2, m_Ppoint.getCoord(0)/(m_IA1*sin(m_P.getCoord(0))) * ( m_IA1- (m_I3-m_IA1)*cos(m_P.getCoord(0))
+    - m_I3*m_Ppoint.getCoord(2)*cos(m_P.getCoord(0))) ) ;
+
+
+
+    return sortie;
+
+}
 
