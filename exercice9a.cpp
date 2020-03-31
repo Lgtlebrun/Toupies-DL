ed43999f05bb42998e4fa4e0a5304a4b2c614ef5
David-Rossboth <david.rossboth@epfl.ch>
2020-03-31 18:32:07 +0100

    0.86% d'erreur dans l'exercice 8

--- exercice9a.cpp
+++ exercice9a.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main(){
 
-    Bille B(0,1,1,2);
-    Oscillateur O(Vecteur({1}),Vecteur({0}));
+    Bille B(0.0,1.0,1.0,2.0);
+    Oscillateur O(Vecteur({1.0}),Vecteur({0.0}));
 
     const double dt(0.01);
 
@@ -15,30 +15,27 @@ int main(){
 
     IntegrateurEulerCromer I(0.0);
 
-    if (flux.fail()) {
+    if (!flux.fail()) {
 
-        TextViewer T(cout);
-
-    } else {
 
         TextViewer T(flux);
 
+
+        Systeme S(T, I);
+
+        S.addIntegrable(&B);
+        S.addIntegrable(&O);
+
+
+        for (int k(0); k < 68; ++k) {
+
+            S.evolue(dt);
+            S.dessine();
+
+        }
+
+        flux.close();
     }
-
-    Systeme S(&T, &I);
-
-    S.addIntegrable(B);
-    S.addIntegrable(O);
-
-
-    for (int k(0); k<68; ++k) {
-
-        S.evolue(dt);
-        S.dessine();
-
-    }
-
-    flux.close();
 
     return 0;
 }
--- exercice9b.cpp
+++ exercice9b.cpp
@@ -7,41 +7,39 @@ using namespace std;
 
 int main() {
 
-    ConeSimple C1({0,M_PI/6, 0}, {0,0,60}, 0.5, 1.5, 0.1);
-    ConeSimple C2({0,M_PI/4, 0}, {0,0,40}, 0.5, 1.5, 0.1);
+    ConeSimple C1({M_PI/6,0, 0}, {0,0,60}, 0.5, 1.5, 0.1);
+    ConeSimple C2({M_PI/4,0, 0}, {0,0,40}, 0.5, 1.5, 0.1);
 
     const double dt(0.01);
 
-    string Exo("Exercice9a.txt");
+    string Exo("Exercice9b.txt");
     ofstream flux (Exo.c_str());
 
-    IntegrateurEulerCromer I(0.0);
+    IntegrateurNewmark I(0.0);
 
-    if (flux.fail()) {
+    if (!flux.fail()) {
 
-        TextViewer T(cout);
-
-    } else {
 
         TextViewer T(flux);
 
+
+        Systeme S(T, I);
+
+        S.addIntegrable(&C1);
+        S.addIntegrable(&C2);
+
+        S.affiche(flux);
+
+        for (int k(0); k < 19 ; ++k) {
+
+            S.evolue(dt);
+            S.dessine();
+
+        }
+
+        flux.close();
+
     }
-
-    Systeme S(&T, &I);
-
-    S.addIntegrable(C1);
-    S.addIntegrable(C2);
-
-    S.affiche(flux);
-
-    for (int k(0); k<68; ++k){
-
-        S.evolue(dt);
-        S.dessine();
-
-    }
-
-    flux.close();
 
     return 0;
 }
