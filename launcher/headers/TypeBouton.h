bb5f9cf36378375288ad4acd75d565145cba78af
Léonard LEBRUN <leonard.lebrun@epfl.ch>
2020-04-15 01:17:21 +0200

    deuxieme launcher

--- launcher/headers/TypeBouton.h
+++ launcher/headers/TypeBouton.h
@@ -1,13 +1,47 @@
 #include <QPushButton>
+#include <QDoubleSpinBox>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QMovie>
+
 
 #ifndef TYPEBOUTON_H
 #define TYPEBOUTON_H
 
+enum Type {CONE, CHINOISE, OSCILLATEUR, BILLE, Null};
+
 class TypeBouton : public QPushButton {
 
+    Q_OBJECT
+
 public:
+    TypeBouton(Type, QWidget* = nullptr);
 
+    bool isPresse()const {return m_pressed;}
+    Type getType() const {return m_type;}
 
+    double getRayon() {return m_barRayon->value();}
+    double getHauteur() {return m_barHauteur->value();}
+    double getMVol(){return m_barMVol->value();}
+
+    void setPresse(bool);
+
+    void changeStatut();
+    void changeSkin();
+
+signals:
+
+    void statutChange();
+
+private:
+
+    bool m_pressed;
+
+    QLabel* m_photo;
+    Type m_type;
+    QDoubleSpinBox* m_barRayon;
+    QDoubleSpinBox* m_barHauteur;
+    QDoubleSpinBox* m_barMVol;
 
 };
 
