f7dbd866c4b64fd96b72e5499c5478133dacd4d6
Léonard LEBRUN <leonard.lebrun@epfl.ch>
2020-04-15 15:47:16 +0200

    tb.h

--- launcher/headers/TypeBouton.h
+++ launcher/headers/TypeBouton.h
@@ -24,6 +24,19 @@ public:
     double getHauteur() {return m_barHauteur->value();}
     double getMVol(){return m_barMVol->value();}
 
+    double getPX() const{return m_pX->value();}
+    double getPY() const{return m_pY->value();}
+    double getPZ() const{return m_pZ->value();}
+
+    double getVX() const{return m_vX->value();}
+    double getVY() const{return m_vY->value();}
+    double getVZ() const{return m_vZ->value();}
+
+
+
+
+
+
     void setPresse(bool);
 
     void changeStatut();
@@ -42,6 +55,16 @@ private:
     QDoubleSpinBox* m_barRayon;
     QDoubleSpinBox* m_barHauteur;
     QDoubleSpinBox* m_barMVol;
+
+    QLabel* m_LabParam;
+
+    QDoubleSpinBox* m_pX;
+    QDoubleSpinBox* m_pY;
+    QDoubleSpinBox* m_pZ;
+
+    QDoubleSpinBox* m_vX;
+    QDoubleSpinBox* m_vY;
+    QDoubleSpinBox* m_vZ;
 
 };
 
