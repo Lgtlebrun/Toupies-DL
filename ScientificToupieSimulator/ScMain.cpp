763f4808c31a084d3b5eb0a435ba47b19af34761
Léonard LEBRUN <leonard.lebrun@epfl.ch>
2020-05-11 13:45:03 +0200

    pour le D

--- ScientificToupieSimulator/ScMain.cpp
+++ ScientificToupieSimulator/ScMain.cpp
@@ -1,11 +1,11 @@
-#include "widget.h"
+#include "headers/ScWidget.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
+    ScWidget w;
     w.show();
     return a.exec();
 }
